c2f7b9586ec91b5b86c27203f1bf7033d69c53aa
rekakovacs97 <74371358+rekakovacs97@users.noreply.github.com>
2021-11-23 14:04:30 +0200

    learning for and if

--- myJS.js
+++ myJS.js
@@ -3,6 +3,7 @@ document.getElementById("info").addEventListener("click", function() {
     document.getElementById("info").innerHTML = "Hello Worldsdddsfsdd";
     document.getElementById("bla").innerHTML = "Blalba";
     document.getElementById("bla").style.color = "red";
+
   });
 
 
@@ -35,9 +36,48 @@ let yourName = 'Joe' //string
 let name = {fist: 'Jane' , last: 'Doe'}; //object
 let truth = false;  //boolean
 let groceries = ['apple' , 'banana' , 'tomato']; //array 
+let car //undefined
+let nothing = null //null
 
 console.log(groceries);
 console.log(groceries.slice(2,6));
+
+//Strings in JS (common methods)
+
+let fruit = 'banana, apple';
+let moreFruit = 'banana\nadssdfdspple'; //new line \n
+
+console.log(fruit.length);
+console.log(moreFruit);
+console.log(fruit.indexOf('nan'));
+console.log(fruit.slice(2 , 6));               //kiirja a 2tol a 6. betuig
+console.log(fruit.replace('ana' , '1234'));   //replace ana to 1234
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());
+console.log(fruit.charAt(2));  //same    kiirja a masodik betut
+console.log(fruit[2]);               //same
+console.log(fruit.split(''));        // split by chracter  - vesszokkel kiirja, kulon a betuket
+console.log(fruit.split(',')); 
+
+//Array
+let fruits = ['banana' , 'apple' , 'orange' , 'pineapples'] //a way to create an array
+
+fruits = new Array('banana' , 'apple' , 'orange' , 'pineapples')  //other ay to creata an array
+
+//alert(fruits[1]);     //acces valuea at index 1 
+fruits[0] = 'pear'; //change the value
+console.log(fruits);
+for(let i = 0; i < fruits.length; i++) { //i=0 a 0.a pear 0 kisebb mint 4 (4elem=length, 0+1 )
+  console.log(fruits[i]);
+}
+
+function onFruitsClick(fruit) {
+ for (let i = 0; i < fruits.length; i++ ) {
+   if(fruit === fruits[i]) {
+     console.log('found the following fruit: ' + fruit);
+   }
+ } 
+}
 
 
 
